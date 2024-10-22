253f47939cbd68b397a730a64c267736b0280dc1
Omitul <omitul1010@gmail.com>
2024-10-22 11:50:00 +0600

    fixed

--- components/introCard.tsx
+++ components/introCard.tsx
@@ -5,7 +5,9 @@ import { Button } from "@nextui-org/button";
 const Introcard = () => {
   return (
     <div className="gap-y-10">
-      <h1 className="text-7xl font-bold w-full mb-4">Hi, I'm Omitul Islam</h1>
+      <h1 className="text-7xl font-bold w-full mb-4">
+        Hi, I&apos;m Omitul Islam
+      </h1>
       <p className="text-3xl text-gray-500 mb-4">
         Problem Solver | Web Technology Enthusiast | 3* at codeshef (max: 1788)
         | Expert in Data Structures, Algorithms & OOP | MERN
