f30df7368d98803386ae229763059f27ce828501
Omitul <omitul1010@gmail.com>
2024-10-23 17:38:08 +0600

    added pdf download for resume using dummy pdf

--- components/introCard.tsx
+++ components/introCard.tsx
@@ -2,6 +2,16 @@
 
 import { Button } from "@nextui-org/button";
 
+const handleDownload = (e: React.MouseEvent) => {
+  e.preventDefault();
+  const link = document.createElement("a");
+  link.href = "/resume/test.pdf";
+  link.download = "Resume_Omitul_CoU.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
 const Introcard = () => {
   return (
     <div className="gap-y-10">
@@ -12,16 +22,25 @@ const Introcard = () => {
         Problem Solver | Web Technology Enthusiast | 3* at codeshef (max: 1788)
         | Expert in Data Structures, Algorithms & OOP | MERN
       </p>
-      <Button
-        className="bg-blue-500 font-semibold"
-        onClick={() => {
-          document
-            .getElementById("contact")
-            ?.scrollIntoView({ behavior: "smooth" });
-        }}
-      >
-        CONTACT ME
-      </Button>
+      <div className="flex flex-row gap-x-5 items-center">
+        <Button
+          className="bg-blue-500 font-semibold"
+          onClick={() => {
+            document
+              .getElementById("contact")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          CONTACT ME
+        </Button>
+        <Button
+          className="bg-red-400 font-semibold hover:bg-red-500"
+          href=""
+          onClick={handleDownload}
+        >
+          Resume
+        </Button>
+      </div>
     </div>
   );
 };
